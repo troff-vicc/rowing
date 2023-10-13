cc94aeae3a4bb59a14785d24e793b937f8528865
troff-vicc <118105988+troff-vicc@users.noreply.github.com>
2023-10-13 13:17:10 +0300

    Update settings.py

--- siteRowing/siteRowing/settings.py
+++ siteRowing/siteRowing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y(!fj$^55wacd6)ir@5te+6dn5ch2_%t9%-qwuu)uya)x=)bq-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
