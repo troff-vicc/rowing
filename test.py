b1a8f33ff236a3efb6df86f4652659b516eaab6c
troff-vicc <troff.vicc@gmail.com>
2023-10-29 13:20:20 +0300

    version 0.1.2

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import sqlite3
 import matplotlib.pyplot as plt
-
-con = sqlite3.connect('rowing.db')
-cur = con.cursor()
-cur.execute("SELECT * from rowingFor1")
-row = cur.fetchall()
 name = input()
-name = name[:name.find(' ')] + ',' + name[name.find(' '):]
-print(name)
-sportman = []
-for data in row:
-    if data[8] == name:
-        sportman.append(data)
-for t in sportman:
-    print(t[-1], t[8])
+newName = name[:name.find(',')] + ' ' + name[name.find(',') + 2:]
+print(newName)
